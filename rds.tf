ac4414f4871af94cedd03199aafe9265e427bacf
ABDUL SAMIM MONDAL <mercuryman912@gmail.com>
2025-06-16 14:38:15 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,26 +1,19 @@
-resource "aws_db_subnet_group" "rds" {
-  name       = "rds-subnet-group"
-  subnet_ids = module.vpc.private_subnets
-
-  tags = {
-    Name = "RDS subnet group"
-  }
-}
-
 resource "aws_db_instance" "workout" {
   allocated_storage    = 20
   engine               = "postgres"
-  engine_version       = "14"
+  engine_version       = "15.3"
   instance_class       = "db.t3.micro"
-  name                 = var.db_name
   username             = var.db_username
   password             = var.db_password
-  db_subnet_group_name = aws_db_subnet_group.rds.name
-  skip_final_snapshot  = true
-  publicly_accessible  = false
-  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  db_subnet_group_name = aws_db_subnet_group.workout_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+
+  skip_final_snapshot = true
+  publicly_accessible = false
+
+  identifier = "micro-workout-db"
 
   tags = {
-    Name = "workout-db"
+    Name = "micro-workout-db"
   }
 }
